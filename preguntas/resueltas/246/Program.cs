c98f5727a8f2465d37df32c6da25235792d084b8
Juan Ramón Díaz Fernández <juan_diaz44@hotmail.com>
2020-12-04 00:38:15 +0100

    246

---
+++ preguntas/resueltas/246/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _246
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myData = new List<string>();
+            myData.Add("dato1");
+            myData.Add("dato2");
+            myData.Add("dato3");
+            System.Console.WriteLine($"elementos:{myData.Count} y son :");
+            foreach (string currentString in myData)
+                System.Console.WriteLine(currentString);
+
+            System.Console.WriteLine("iniciamos la eliminación");
+            while (myData.Count != 0)
+                myData.RemoveAt(0); 
+            
+            System.Console.WriteLine($"elementos:{myData.Count}");
+        }
+
+    }
+}
---
+++ preguntas/resueltas/246/readme.md
@@ -0,0 +1,10 @@
+QUESTION 246 __ERRATA__ __ERRATA__ __ERRATA__ __ERRATA__ __ERRATA__
+You have the following code.
+
+![c1](c1.PNG)
+
+CORRECTA
+
+
+            while (myData.Count != 0)
+                myData.RemoveAt(0); 
