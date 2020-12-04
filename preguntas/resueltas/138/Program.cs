58980756da138be4dcb69a51f982fa37799c8244
Juan Ramón Díaz Fernández <juan_diaz44@hotmail.com>
2020-12-04 10:16:25 +0100

    138

---
+++ preguntas/resueltas/138/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _138
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string> () {"12.36","-12.36","abc","12.00","12"};
+            foreach (var item in lista)
+            {
+                opcionc(item);
+            }
+        }
+
+        public static void opcionc( string price){
+            
+            string mensaje = $"Prueba opcion a {price}";
+           
+            Regex reg = new Regex( @"^\d+(\.\d\d)?$");
+            //Regex reg = new Regex( @"^(\d{1}\.)?(\d+\.?)+(,\d{2})?$");
+            if (reg.IsMatch(price)) 
+                System.Console.WriteLine($"{mensaje} valid Price");
+            else
+                  System.Console.WriteLine($"{mensaje} INVALID Price");
+            
+        }
+    }
+}
---
+++ preguntas/resueltas/138/readme.md
@@ -0,0 +1,27 @@
+## QUESTION 138 --ERRATA-- __ERRATA__
+LA RESPUESTA CORRECTA ES LA C
+
+You are creating a console application named App1.  
+App1 will validate user input for order entries.  
+You are developing the following code segment (line numbers are included for reference only):  
+
+
+![c1](c1.PNG)
+
+
+You need to complete the code segment.  
+The solution must ensure that prices are positive and have two decimal places.  
+Which code should you insert at line 03?  
+
+![c2](c2.PNG)
+
+Correct Answer: A __ERRATA__ __ERRATA__ __ERRATA__ __ERRATA__ __ERRATA__
+
+La CORRECTA ES LA C
+
+Si te fijas B y C indican  que puede entrar un signo negativo!  DESCARTADAS
+
+A if !(reg.IsMatch(price)) o sea que si no se cumple entonces price valid !!!!! __ERROR__
+
+C if (reg.IsMatch(price))  si se cumple entonces prive valid OK  
+
