7996f4fda98da6ef10926d132ff66774c2737063
Juan Ramón Díaz Fernández <juan_diaz44@hotmail.com>
2020-12-06 20:15:59 +0100

    67

---
+++ preguntas/resueltas/67/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double x, y;
+            x=0.0; y = 0.0;
+            System.Console.WriteLine($"double (0.00d/0.00d); {x/y}");
+            float xf, yf;
+            xf=0.00f; yf = 0.00f;
+            System.Console.WriteLine($"float  (0.00f/0.00f); {xf/yf}");
+            decimal xd, yd;
+            xd=0.00m; yd = 0.00m;
+            try{
+                System.Console.WriteLine($"decimal (0.00d/0.00d); {xd/yd}");}
+
+            catch (Exception ex )
+            {
+                System.Console.WriteLine($"decimal (0.00d/0.00d); {ex.Message}");  
+            }
+            int xi, yi;
+            xi=0; yi=0;
+           try{
+                System.Console.WriteLine($"integer (0/0); {xi/yi}");
+                }
+
+            catch (Exception ex2 )
+            {
+                System.Console.WriteLine($"integer (0/0); {ex2.Message}");  
+            }
+ 
+
+        }
+    }
+}
---
+++ preguntas/resueltas/67/readme.md
@@ -0,0 +1,25 @@
+# QUESTION 67 __ERRATA__
+
+
+You have the following code. (Line numbers are included for reference only.)
+
+![c1](c1.PNG)
+
+
+What is the output of line 04?
+A. Error
+B. 0
+C. null
+D. NaN
+
+
+
+RESPUESTA A __ERRATA__ __ERRATA__ __ERRATA__ __ERRATA__ __ERRATA__
+
+Respuesta correcta D Nan
+
+
+double (0.00d/0.00d); NaN  
+float  (0.00f/0.00f); NaN  
+decimal (0.00d/0.00d); Attempted to divide by zero.  
+integer (0/0); Attempted to divide by zero.  
